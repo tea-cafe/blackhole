29d1a2789677cc8de1d7d4b92737ea4ebb5cedf7
laophiler <530698728@qq.com>
2017-12-19 20:52:17 +0800

    fix bug

--- ui/controllers/Jsapi.php
+++ ui/controllers/Jsapi.php
@@ -14,18 +14,18 @@ class Jsapi extends MY_Controller {
         $strJs = '';
         $strJs .= "(function(){"."\n";
         $strJs .= "var o = document.getElementsByTagName(\"script\");"."\n";
-        $strJs .= "var c = o[o.length-1].parentNode;";
-        $strJs .= "var ta = document.createElement('script'); ta.type = 'text/javascript'; ta.async = true;";
-        $strJs .= "ta.src = '//yun.lvehaisen.com/h5/media/media-3.2.1.min.js';";
-        $strJs .= "ta.onload = function() {";
-        $strJs .= "new TuiaMedia({";
-        $strJs .= "container: c,";
-        $strJs .= "appKey: '".$arrParams['appKey']."',";
-        $strJs .= "adslotId: '".$arrParams['adslotId']."'";
-        $strJs .= "});";
-        $strJs .= "}";
-        $strJs .= "var s = document.querySelector('head'); s.appendChild(ta);";
-        $strJs .= "})();";
+        $strJs .= "var c = o[o.length-1].parentNode;"."\n";
+        $strJs .= "var ta = document.createElement('script'); ta.type = 'text/javascript'; ta.async = true;"."\n";
+        $strJs .= "ta.src = '//yun.lvehaisen.com/h5/media/media-3.2.1.min.js';"."\n";
+        $strJs .= "ta.onload = function() {"."\n";
+        $strJs .= "new TuiaMedia({"."\n";
+        $strJs .= "container: c,"."\n";
+        $strJs .= "appKey: '".$arrParams['appKey']."',"."\n";
+        $strJs .= "adslotId: '".$arrParams['adslotId']."'"."\n";
+        $strJs .= "});"."\n";
+        $strJs .= "};"."\n";
+        $strJs .= "var s = document.querySelector('head'); s.appendChild(ta);"."\n";
+        $strJs .= "})();"."\n";
         echo $strJs;
 
     }
