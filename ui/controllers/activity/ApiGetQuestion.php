7c701b04a37bf12461518131cfe0a467d103762d
tea-cafe <tea_cafe@163.com>
2018-01-11 12:01:36 +0800

    add /activity/ApiGetQuestion

--- ui/controllers/activity/ApiGetQuestion.php
+++ ui/controllers/activity/ApiGetQuestion.php
@@ -1,15 +1,26 @@
 <?php
 class ApiGetQuestion extends MY_Controller {
+
+    const ARR_TYPES = [
+        '历史文艺',
+        '疯狂科学',
+        '清奇脑洞',
+        '趣问八卦',
+    ];
+
     public function __construct() {
         parent::__construct();
     }
 
     public function index() {
         $this->load->config('activity_question');
+        $strType = self::ARR_TYPES[mt_rand(0,3)];
         $arrQuestions = $this->config->item('activity_question');
-        $arrQuestion = $arrQuestions[mt_rand(1,2)][mt_rand(0,99)];
+        $arrQuestions = $arrQuestions[$strType];
+        $arrQuestion = $arrQuestions[mt_rand(0,count($arrQuestions))];
         if (mt_rand(0,1)) {
             $arrRes = [
+                'type' => $strType,
                 'question' => $arrQuestion[0],
                 'A' => $arrQuestion[2],
                 'B' => $arrQuestion[1],
@@ -17,6 +28,7 @@ class ApiGetQuestion extends MY_Controller {
             ];
         } else {
             $arrRes = [
+                'type' => $strType,
                 'question' => $arrQuestion[0],
                 'A' => $arrQuestion[1],
                 'B' => $arrQuestion[2],
