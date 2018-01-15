daa59df6ae1f0541cc8cb07bee14949fe5f17ae7
tea-cafe <tea_cafe@163.com>
2018-01-15 15:46:24 +0800

    question final version

--- ui/controllers/activity/ApiGetQuestion.php
+++ ui/controllers/activity/ApiGetQuestion.php
@@ -24,7 +24,7 @@ class ApiGetQuestion extends MY_Controller {
                 'question' => $arrQuestion[0],
                 'A' => $arrQuestion[2],
                 'B' => $arrQuestion[1],
-                'search_url' => $arrQuestion[3],
+                'search_url' => 'https://m.baidu.com/s?wd=' . urlencode($arrQuestion[3]) . '&ref=www_colorful&tn=iphone&from=1018279e',
                 'r' => 'B',
             ];
         } else {
@@ -33,7 +33,7 @@ class ApiGetQuestion extends MY_Controller {
                 'question' => $arrQuestion[0],
                 'A' => $arrQuestion[1],
                 'B' => $arrQuestion[2],
-                'search_url' => $arrQuestion[3],
+                'search_url' => 'https://m.baidu.com/s?wd=' . urlencode($arrQuestion[3]) . '&ref=www_colorful&tn=iphone&from=1018279e',
                 'r' => 'A',
             ];
         }
